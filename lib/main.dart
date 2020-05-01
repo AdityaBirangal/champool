aff11f6f38fc43f0fba790cf1825353369ff67ae
Aditya Birangal <aditya.birangal@gmail.com>
2020-05-02 01:27:51 +0530

    Play Screen,Option Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:champool/screen/playScreen.dart';
+import 'package:champool/screen/optionScreen.dart';
 
 void main() {
-  runApp(ChampoolApp());
+  runApp(MaterialApp(
+    home: ChampoolApp(),
+  ));
 }
 
 class ChampoolApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset("images/Champool_Logo.png"),
-              SizedBox(
-                height: 20,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/Champool_Logo.png"),
+            SizedBox(height: 10),
+            FlatButton(
+              padding: EdgeInsets.all(5),
+              color: Colors.greenAccent,
+              child: Text(
+                "    Play    ",
+                style: TextStyle(fontSize: 30),
               ),
-              FlatButton(
-                padding: EdgeInsets.all(10),
-                color: Colors.greenAccent,
-                child: Text(
-                  "    Play    ",
-                  style: TextStyle(fontSize: 30),
-                ),
-                onPressed: () {},
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PlayScreen()));
+              },
+            ),
+            SizedBox(height: 10),
+            FlatButton(
+              padding: EdgeInsets.all(5),
+              color: Colors.greenAccent,
+              child: Text(
+                "    Option    ",
+                style: TextStyle(fontSize: 30),
               ),
-              SizedBox(
-                height: 20,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => OptionScreen()));
+              },
+            ),
+            SizedBox(height: 10),
+            FlatButton(
+              padding: EdgeInsets.all(5),
+              color: Colors.greenAccent,
+              child: Text(
+                "    Help    ",
+                style: TextStyle(fontSize: 30),
               ),
-              FlatButton(
-                padding: EdgeInsets.all(10),
-                color: Colors.greenAccent,
-                child: Text(
-                  "    Option    ",
-                  style: TextStyle(fontSize: 30),
-                ),
-                onPressed: () {},
+              onPressed: () {},
+            ),
+            SizedBox(height: 10),
+            FlatButton(
+              padding: EdgeInsets.all(5),
+              color: Colors.greenAccent,
+              child: Text(
+                "    About    ",
+                style: TextStyle(fontSize: 30),
               ),
-              SizedBox(
-                height: 20,
+              onPressed: () {},
+            ),
+            SizedBox(height: 10),
+            FlatButton(
+              padding: EdgeInsets.all(5),
+              color: Colors.greenAccent,
+              child: Text(
+                "    Exit    ",
+                style: TextStyle(fontSize: 30),
               ),
-              FlatButton(
-                padding: EdgeInsets.all(10),
-                color: Colors.greenAccent,
-                child: Text(
-                  "    Help    ",
-                  style: TextStyle(fontSize: 30),
-                ),
-                onPressed: () {},
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButton(
-                padding: EdgeInsets.all(10),
-                color: Colors.greenAccent,
-                child: Text(
-                  "    About    ",
-                  style: TextStyle(fontSize: 30),
-                ),
-                onPressed: () {},
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButton(
-                padding: EdgeInsets.all(10),
-                color: Colors.greenAccent,
-                child: Text(
-                  "    Exit    ",
-                  style: TextStyle(fontSize: 30),
-                ),
-                onPressed: () {},
-              ),
-            ],
-          ),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
