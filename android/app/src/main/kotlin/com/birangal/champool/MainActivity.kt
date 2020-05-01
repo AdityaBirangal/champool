5c6f1a6ab0f6ee68ce83d291c5ba6fb2977ff224
Aditya Birangal <aditya.birangal@gmail.com>
2020-05-01 23:18:26 +0530

    Init

---
+++ android/app/src/main/kotlin/com/birangal/champool/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.birangal.champool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChampoolApp());
+}
+
+class ChampoolApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
